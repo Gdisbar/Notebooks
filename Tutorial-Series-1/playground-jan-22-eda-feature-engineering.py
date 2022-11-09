4c95f1b41921c67d01fd46e06c01ee1483223f5c
Gdisbar <115510224+Gdisbar@users.noreply.github.com>
2022-11-09 16:29:59 +0530

    Add files via upload

--- Tutorial-Series-1/playground-jan-22-eda-feature-engineering.py
+++ Tutorial-Series-1/playground-jan-22-eda-feature-engineering.py
@@ -13,12 +13,67 @@ pearsonr(train.loc[train['product']=='Kaggle Mug','num_sold'], train.loc[conditi
 from pmdarima import auto_arima
 import statsmodels.api as sm
 
-mod = sm.tsa.statespace.SARIMAX(train.loc[train['product']=='Kaggle Mug','num_sold'],
-                                order=(1,1,1),
-                                seasonal_order=(1, 1, 1, 7))
+mod = sm.tsa.statespace.SARIMAX(...)
 results = mod.fit(disp=False)
 display(results.summary().tables[1])
-
-sns.set(font_scale=1.5)
 results.plot_diagnostics(figsize=(16, 8))
-plt.show()
+
+import datetime as dt
+
+def get_ts_dict(df):
+	# for country_list ,store_list,product_list -> time_series_dict[key] = selected_pd
+	# here key is created using combining 3 elements from the above list
+
+ts_dict = get_ts_dict(train)
+key_list = list(ts_dict.keys())
+
+def ts_plot(ts_dict, key_list, figsize=(24, 24)):
+    # for every row & col use index to create df & row,col for axes
+    # key -> key_list[index] --> 
+    # df = ts_dict[key]
+    # lineplot -> df,'date','num_sold'
+	# x_label -> date_range
+	# axvline -> df['date'].dt.year
+            
+
+from scipy.signal import periodogram
+
+def plot_periodogram(ts, detrend='linear', ax=None, title=''):
+	fs = pd.Timedelta("1Y") / pd.Timedelta("1D")
+	# fs -> ratio of '1Y' & '1D' in Date scale
+    #freqencies, spectrum = periodogram(...)
+    
+
+import holidays
+import datetime
+
+def feature_eng(df):
+    #### Date
+    #### Peak
+    #### Till The Next Holiday
+    def get_country_holidays(country, years_list):
+    	# festivities -> get each country holiday list
+    	# festivities_df -> df using festivities
+    	# festivities_df['date'] -> convert datetime() format
+    	# festivities_df -> update for any specific festival without holiday
+    	# additional_dates -> common to all not included in holiday calendar
+        # additional_festivities_df -> add additional_dates to festivities_df
+        # append additional_festivities_df to festivities_df + sort by 'date'
+        
+
+    def days_till_next_holiday(country, date):
+    	# country_holidays_dates -> get complete customized list of holidays for each country
+    	# return min number of days remaining for next holiday
+        
+    # apply on df['days_till_next_holiday']
+    #### Seasonality
+    # date_range -> we're assuming season transition to be 60 days
+    # use fourier on this dataset -> in_sample(),lag(),drop(),merge()
+    
+    #### GDP
+    # gdp_melt_df -> get year wise gdp value for list of countries
+    # df -> merge gdp_melt_df
+    
+    #### GDP Percentage Change Between Years
+    # same like above but here gdp% is calculated
+    # drop unnecessary cols
